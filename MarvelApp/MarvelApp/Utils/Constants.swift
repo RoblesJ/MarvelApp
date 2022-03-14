e556088c2c372bafdf7d8d8d59a4aef822cab2b3
JuliusGlob <julio.robles@globant.com>
2022-03-13 22:15:34 -0300

    Setting up TabController UI

--- MarvelApp/MarvelApp/Utils/Constants.swift
+++ MarvelApp/MarvelApp/Utils/Constants.swift
@@ -5,7 +5,7 @@
 //  Created by Julio Ismael Robles on 13/03/2022.
 //
 
-import Foundation
+import UIKit
 
 struct AuthenticationConstants {
     static let email: String = "Email"
@@ -18,3 +18,34 @@ struct AuthenticationConstants {
     static let signUp: String = "Sign Up"
 }
 
+struct MainTabControllerConstants {
+    static let characterIconFilled: UIImage = {
+        guard let image = UIImage(systemName: "person.3.fill") else { return #imageLiteral(resourceName: "first") }
+        return image
+    }()
+    
+    static let characterIconUnfilled: UIImage = {
+        guard let image = UIImage(systemName: "person.3") else { return #imageLiteral(resourceName: "first") }
+        return image
+    }()
+    
+    static let eventsIconFilled: UIImage = {
+        guard let image = UIImage(systemName: "bookmark.fill") else { return #imageLiteral(resourceName: "first") }
+        return image
+    }()
+    
+    static let eventsIconUnfilled: UIImage = {
+        guard let image = UIImage(systemName: "bookmark") else { return #imageLiteral(resourceName: "first") }
+        return image
+    }()
+    
+    static let profileIconFilled: UIImage = {
+        guard let image = UIImage(systemName: "person.crop.circle.fill") else { return #imageLiteral(resourceName: "first") }
+        return image
+    }()
+    
+    static let profileIconUnfilled: UIImage = {
+        guard let image = UIImage(systemName: "person.crop.circle") else { return #imageLiteral(resourceName: "first") }
+        return image
+    }()
+}
